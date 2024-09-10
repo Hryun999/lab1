0da8e5fba43e62aec9551bf0868362a7889ab162
Hryun999 <ilahbrv2@gmail.com>
2024-09-10 14:21:05 +0300

    task1

---
+++ 1.1/1.1/1.1.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int mass[10] = { 10, 15, 3, 8, 64, 124, 55, 20, 19, 13 };
+    int max = INT_MIN;
+    int min = INT_MAX;
+    for (int i = 0; i < 10; i++) {
+        if (mass[i] < min) {
+            min = mass[i];
+        }
+        if (mass[i] > max) {
+            max = mass[i];
+        }
+    }
+
+    printf("max: %d - min: %d = %d\n", max, min, max - min);
+    return 0;
+}
