7f45608f9ba8ec8cdb6b1a331bb8e1d907f882b0
Hryun999 <ilahbrv2@gmail.com>
2024-09-10 14:23:49 +0300

    task2

--- 1.1/1.1/1.1.c
+++ 1.1/1.1/1.1.c
@@ -1,10 +1,23 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
+#include <time.h>
 
 int main() {
-    int mass[10] = { 10, 15, 3, 8, 64, 124, 55, 20, 19, 13 };
+    int mass[10] = { 0 };
     int max = INT_MIN;
     int min = INT_MAX;
+
+    srand(time(NULL));
+
+    printf("mass: ");
+
+    for (int i = 0; i < 10; i++) {
+        mass[i] = rand() % 150 - 75;
+        printf("%d ", mass[i]);
+    }
+    printf("\n");
+
     for (int i = 0; i < 10; i++) {
         if (mass[i] < min) {
             min = mass[i];
@@ -14,6 +27,7 @@ int main() {
         }
     }
 
-    printf("max: %d - min: %d = %d\n", max, min, max - min);
+    printf("max: %d min: %d\n", max, min);
+
     return 0;
 }
