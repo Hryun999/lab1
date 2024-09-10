599bea98df9a6d548d10e028efadd5769a474568
Hryun999 <ilahbrv2@gmail.com>
2024-09-10 14:34:57 +0300

    task4

--- 1.1/1.1/1.1.c
+++ 1.1/1.1/1.1.c
@@ -4,16 +4,16 @@
 #include <time.h>
 
 int main() {
-    int size;
+    int rows, columns;
 
-    printf("Enter size of array: ");
-    scanf_s("%d", &size);
+    printf("Enter the number of rows: ");
+    scanf_s("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf_s("%d", &columns);
 
-    int* array = (int*)malloc(size * sizeof(int));
-
-    if (array == NULL) {
-        printf("Memory allocation failed\n");
-        return 1;
+    int** array = (int**)malloc(rows * sizeof(int*));
+    for (int i = 0; i < rows; i++) {
+        array[i] = (int*)malloc(columns * sizeof(int));
     }
 
     int max = INT_MIN;
@@ -21,25 +21,44 @@ int main() {
 
     srand(time(NULL));
 
-    printf("mass: ");
+    printf("mass:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            array[i][j] = rand() % 150 - 75;
+            printf("%d ", array[i][j]);
 
-    for (int i = 0; i < size; i++) {
-        array[i] = rand() % 150 - 75;
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    for (int i = 0; i < size; i++) {
-        if (array[i] < min) {
-            min = array[i];
+            if (array[i][j] < min) {
+                min = array[i][j];
+            }
+            if (array[i][j] > max) {
+                max = array[i][j];
+            }
         }
-        if (array[i] > max) {
-            max = array[i];
-        }
+        printf("\n");
     }
 
     printf("max: %d min: %d\n", max, min);
 
+    for (int i = 0; i < rows; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < columns; j++) {
+            rowSum += array[i][j];
+        }
+        printf("rowSum: %d\n", rowSum);
+    }
+
+    for (int j = 0; j < columns; j++) {
+        int columnSum = 0;
+        for (int i = 0; i < rows; i++) {
+            columnSum += array[i][j];
+        }
+        printf("columnSum: %d\n", columnSum);
+    }
+
+    for (int i = 0; i < rows; ++i) {
+        free(array[i]);
+    }
     free(array);
+
     return 0;
 }
